8f10482b51fab64711f632b65f177b61423db2b4
Oliwier965 <olikucz@vp.pl>
2021-04-05 15:20:13 +0200

    Generate button

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const sliders = document.querySelectorAll('input[type="range"');
 const currentHexes = document.querySelectorAll(".color h2");
 const popup = document.querySelector(".copy-container");
 const adjustButton = document.querySelectorAll(".adjust");
+const lockButton = document.querySelectorAll(".lock");
 const closeAdjustments = document.querySelectorAll(".close-adjustment");
 const sliderContainers = document.querySelectorAll(".sliders");
 let initialColors;
 //Add event lsiteners
+generateBtn.addEventListener("click", randomColors);
+
 sliders.forEach((slider) => {
   slider.addEventListener("input", hslControls);
 });
@@ -73,6 +76,11 @@ function randomColors() {
   });
   //Reset input
   resetInputs();
+  //Check for button contrast
+  adjustButton.forEach((button, index) => {
+    checkTextContrast(initialColors[index], button);
+    checkTextContrast(initialColors[index], lockButton[index]);
+  });
 }
 
 function checkTextContrast(color, text) {
