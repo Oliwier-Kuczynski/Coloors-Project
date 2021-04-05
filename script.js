dbf3f5fb9172656ca57968ccdf4b5e4b537c44a4
Oliwier965 <olikucz@vp.pl>
2021-04-05 14:36:55 +0200

    Added copy to clipboard functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"');
 const currentHexes = document.querySelectorAll(".color h2");
+const popup = document.querySelector(".copy-container");
 let initialColors;
 //Add event lsiteners
 sliders.forEach((slider) => {
@@ -13,6 +14,18 @@ colorDivs.forEach((div, index) => {
   div.addEventListener("change", () => {
     updateTextUi(index);
   });
+});
+
+currentHexes.forEach((hex) => {
+  hex.addEventListener("click", () => {
+    copyToClipboard(hex);
+  });
+});
+
+popup.addEventListener("transitionend", () => {
+  const popupBox = popup.children[0];
+  popupBox.classList.remove("active");
+  popup.classList.remove("active");
 });
 
 //Functions
@@ -133,6 +146,19 @@ function resetInputs() {
       slider.value = Math.floor(satValue * 100) / 100;
     }
   });
+}
+
+function copyToClipboard(hex) {
+  const el = document.createElement("textarea");
+  el.value = hex.textContent;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  //Popup animation
+  const popupBox = popup.children[0];
+  popupBox.classList.add("active");
+  popup.classList.add("active");
 }
 
 randomColors();
